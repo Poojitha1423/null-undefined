3ce7dca0338348d8282746851710a12fe903caed
Poojitha <poojitha032320@gmail.com>
2022-08-17 17:12:54 +0530

    null,undefined

---
+++ Undefined.js
@@ -0,0 +1,8 @@
+var myVar;
+console.log(myVar);
+
+var name;
+console.log(myVar);
+
+var name52;
+console.log(name52);
---
+++ null.js
@@ -0,0 +1,8 @@
+var myVar = null;
+console.log(myVar);
+
+var myname = null;
+console.log(myname);
+
+var personname = null;
+console.log(personname);
